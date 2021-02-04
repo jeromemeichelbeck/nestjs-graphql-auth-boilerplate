b76c7f3f077a8719aceaefb7e75a79fe975e6cba
Jérôme Meichelbeck <jerome@meichelbeck.io>
2021-02-04 23:14:41 +0100

    auth guard

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,11 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common'
+import { GqlExecutionContext } from '@nestjs/graphql'
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    canActivate(context: ExecutionContext): boolean {
+        const ctx = GqlExecutionContext.create(context)
+
+        return !!ctx.getContext().req.session.userId
+    }
+}
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,6 @@
+import { UseGuards } from '@nestjs/common'
 import { Query, Resolver } from '@nestjs/graphql'
+import { AuthGuard } from '../guards/auth.guard'
 import { User } from './user.entity'
 import { UsersService } from './users.service'
 
@@ -7,6 +9,7 @@ export class UsersResolver {
     constructor(private readonly usersService: UsersService) {}
 
     @Query(() => [User])
+    @UseGuards(AuthGuard)
     async users(): Promise<User[]> {
         return this.usersService.getUsers()
     }
